2ea14533ab6c2d9de17b910e78bc74bbb2ce700a
hackingjpr <jameshacking1@gmail.com>
2022-05-04 12:20:25 +0000

    hi

--- ShinyAppScript.R
+++ ShinyAppScript.R
@@ -41,35 +41,11 @@ ui <- shiny::fluidPage(
   mainPanel(
   tabsetPanel(
     tabPanel(
-      "Inputs",
+      "Info",
       (fluidRow(
-        # fileInput(
-        #   "idatFile",
-        #   "Upload idat file:",
-        #   multiple = TRUE,
-        #   accept = ".idat"
-        # ),
-        # selectInput("metagenes", "Metagenes", c("ALL", "ATRT", "ECRT")),
-        # # column(
-        # #   3,
-        #   actionBttn(
-        #     inputId = "bttn1",
-        #     label = "M value me!",
-        #     color = "primary",
-        #     style = "fill"
-        #   ),
-        # # ),
-        # # column(
-        # #   3,
-        # #   actionBttn(
-        # #     inputId = "bttn2",
-        # #     label = "RESULTS?!",
-        # #     color = "success",
-        # #     style = "stretch"
-        # #   )
-        # # ),
-        # 
-        "Here is where I will talk about what you can do and what it is all about etc."
+
+        "Here is where I will talk about what you can do and what it is all about etc.",
+        textOutput("time")
       )
     )
     ),
@@ -98,16 +74,34 @@ server <- function(input, output) {
   options(shiny.maxRequestSize = 30 * 1024 ^ 2)
   observeEvent(input$bttn1, {
     req(input$idatFile)
-    
+    require(R.utils)
+    tempdir() -> tempDIR
     
     withProgress(message = 'beep boop, doing basey things', value = 1, {
+      input$idatFile$datapath -> in.files
+      message(in.files)
+      paste0(tempDIR,"/", input$idatFile$name) -> out.files
+      message(out.files)
+      # files = input$idatFile
+      # file = files$datapath[files$name == file]
+      #file.copy(input$idatFile$datapath, paste0("~/Idats/temp/", input$idatFile$name))
+      copied <- file.copy(in.files, out.files)
+      
+      #createLink(in.files, out.files)
+      
+      # idatFile <- input$idatFile
+      # cat("input file is here:\n")
+      # cat(idatFile$datapath, "\n")
       #CANT GET THIS PART TO WORK!!!!
       # X <- read.table(input$idatFile$datapath, sep=",", skipNul = T)
-      # Y <- input$idatFile$datapath
-      # temp.base <- get_basenames(Y)})
-      temp.base <- get_basenames("/home/njh264/Idats/idats_sdf_130213/")
-    })
+
+      temp.base <- get_basenames(tempDIR)})
+    #   temp.base <- get_basenames("/home/njh264/Idats/Mix/")
+    # })
     withProgress(message = "beep boop, doing idat things, trust me it will finish I just haven't worked out how to link the loading bar to progress", value = 0, {
+      
+      cat("Timing start\n")
+      ptm <- proc.time()
       # for (i in 1:150){
       # incProgress(amount = 1/150)
       #   Sys.sleep(1)}
@@ -138,10 +132,13 @@ server <- function(input, output) {
         )
       })}),
       options = list(
-        pageLength = 5,
-        initComplete = I("function(settings, json) {alert('Done.');}")
-      )
+        pageLength = 100,
+        initComplete = I("function(settings, json) {alert('Done.');}")),
+      
+      # times<- (proc.time() - ptm),
+
     )
+    output$time <- renderText({proc.time() - ptm})
   })
   # observeEvent(input$bttn2, {
   #   output$Mval <- renderPrint({
